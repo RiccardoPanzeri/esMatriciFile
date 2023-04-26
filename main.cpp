1adbb354925a83e1670936ba5bd0cd8d9ccbae1e
RiccardoPanzeri <riccardo.panzeri@outlook.it>
2023-04-27 01:06:05 +0200

    aggiornati commenti

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ int main() {
 	scriviM(m, righe, colonne);
 	//riacquisisco la dimensione del file ora che ci ho scritto sopra la matrice:
 	dimensioneFile = dimFile();
-	printf("\n\nOra, la dimensione del file è: %d\n\n\n", dimensioneFile); // ora contando 1 byte per ogni char del file, abbiamo: 1 byte per ogni cifra, 1 byte per ogni spazio (uno dopo ogni cifra), un byte per ogni a capo. Il totale è quindi di 12 byte x 5 righe = 60 byte;  
+	printf("\n\nOra, la dimensione del file è: %d\n\n\n", dimensioneFile); // ora contando 1 byte per ogni char del file, abbiamo: 1 byte per ogni cifra, 1 byte per ogni spazio (uno dopo ogni cifra), un byte per ogni a capo. Il totale è quindi di 11 byte x 5 righe = 55 byte;  
 	//ora provo a leggere la matrice dal file e ainserirla in una seconda matrice vuota;
 	printf("Visualizzo la matrice letta dal file:\n\n");
 	leggiM(m1, righe, colonne);
